488fa64d09426f6c294cbdfdb3876191af61490b
diogo ramos <diogoramos@diogos-Air.Home>
2023-09-01 12:47:33 +0100

    norm

--- src/mit.c
+++ src/mit.c
@@ -12,34 +12,31 @@
 
 #include "../inc/minishell.h"
 
-char **list_to_array(t_list *lst)
+char	**list_to_array(t_list *lst)
 {
-    int count = 0;
-    t_list *tmp = lst;
-    
-    // Count the number of elements in the list
-    while (tmp)
-    {
-        count++;
-        tmp = tmp->next;
-    }
+	int		count;
+	t_list	*tmp;
+	char	**arr; 
+	int		i;
 
-    // Allocate memory for the char ** array
-    char **arr = malloc((count + 1) * sizeof(char *));
-    if (!arr)
-        return NULL; // Memory allocation failed
-
-    int i = 0;
-    tmp = lst;
-    while (tmp)
-    {
-        arr[i] = tmp->content->s;
-        tmp = tmp->next;
-        i++;
-    }
-    arr[i] = NULL; // Null-terminate the array
-
-    return arr;
+	count = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	arr = malloc((count + 1) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	tmp = lst;
+	while (tmp)
+	{
+		arr[i] = tmp->content->s;
+		tmp = tmp->next;
+		i++;
+	}
+	arr[i] = NULL;
+	return (arr);
 }
-
-
